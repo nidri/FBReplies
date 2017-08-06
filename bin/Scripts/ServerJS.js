85b54709755b0b43ea51e3287fdd8010c2e13ea2
santu9 <santosh_369963@yahoo.com>
2017-08-06 17:59:06 -0400

    Added function to get long lived token.

--- bin/Scripts/ServerJS.js
+++ bin/Scripts/ServerJS.js
@@ -1,5 +1,11 @@
 var access_token = "";
 var https = require('https');
+var fs = require('fs');
+var appConfig = JSON.parse(fs.readFileSync('bin/Config/FBAppConfig.json'));
+console.log(appConfig);
+var appId = appConfig.appId;
+var appSecret = appConfig.appSecret;
+console.log("appSecret - " + appSecret);
 exports.ParseRequestParams = function(Request, callback) {
   console.log("Parsing request params");
   //console.log(Request);
@@ -25,7 +31,7 @@ exports.StartReqParsing = function(Body)
   var UserID = Data.UserID;
   access_token = Data.AuthToken;
   console.log("UserID - " + UserID);
-  GetFBDetails();
+  //GetLongLivedToken();
 }
 
 GetFBDetails = function(){
@@ -38,6 +44,26 @@ GetFBDetails = function(){
 
   https.get(options, function(response){
     console.log("FB - " + response.statusCode);
+    response.on('data', function(data){
+      console.log(JSON.parse(data));
+    });
+  });
+};
+
+GetLongLivedToken = function(){
+  //Get long lived token using short expiry token and other details.
+  var options = {
+    method: 'GET',
+    host: 'graph.facebook.com',
+    port: 443,
+    path: '/oauth/access_token?' +
+    'grant_type=fb_exchange_token&amp;' +
+    'client_id=' + appId + '&amp;' +
+    'client_secret=' + appSecret + '&amp;' +
+    'fb_exchange_token=' + access_token
+  };
+  https.get(options, function(response){
+    console.log("FB Long Token - " + response.statusCode);
     response.on('data', function(data){
       console.log(JSON.parse(data));
     });
